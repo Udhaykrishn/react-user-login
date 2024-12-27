918db412ce5a6235d3374c45fc2405eb4bdb8051
ayishathesni <thesni241@gmail.com>
2024-12-27 13:57:07 +0530

    added admin model in mongoose

---
+++ backend/src/models/admin.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const adminSChema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            minLength: 8,
+        },
+        role: {
+            type: String,
+            enum: [ "admin"],
+            default: "admin",
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Admin = mongoose.model("Admin", adminSChema);
+export default Admin;
