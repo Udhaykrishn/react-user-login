4f9f1e42e27afd29c300cf0d8f21848d9af8ee07
arjuxlal <arjunlal543k210@gmail.com>
2024-12-28 21:48:39 +0530

    added the user services in normal way

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,49 +1,54 @@
-import UserService from "#service/user.services.js"
+import UserServices from "#service/user.services.js";
 
 class UserController {
-    #userService;
-    constructor(UserService) {
-        this.#userService = UserService
+  async register(req, res) {
+    try {
+      const payload = req.body;
+      const user = await UserServices.register(payload)
+
+      if (user.success) {
+        return res.status(201).json(user);
+      } else {
+        return res.status(400).json(user);
+      }
+    } catch (error) {
+      console.error("Error in UserController.resiger: ", error.message);
+      return res.status(500).json({
+        success: false,
+        message: "Internal server error",
+      });
     }
-    async register(req, res) {
-        try {
-            const payload = req.body;
-            const user = await this.#userService.register(payload)
+  }
 
-            if (user.success) {
-                return res.status(201).json(user);
-            } else {
-                return res.status(400).json(user);
-            }
-        } catch (error) {
-            console.error("Error in UserController.resiger: ", error.message);
-            return res.status(500).json({
-                success: false,
-                message: "Internal server error"
-            })
-        }
+  async login(req, res) {
+    try {
+      const payload = req.body;
+      const user = await UserServices.login(payload)
+
+      if (user.success) {
+        return res.status(200).json(user);
+      } else {
+        return res.status(401).json(user);
+      }
+    } catch (error) {
+      console.error("Error in UserController.login: ", error.message);
+      return res.status(500).json({
+        success: false,
+        message: "Internal server error",
+      });
     }
+  }
 
-
-    async login(req, res) {
-        try {
-            const payload = req.body;
-            const user = await this.#userService.login(payload)
-
-            if (user.success) {
-                return res.status(200).json(user);
-            } else {
-                return res.status(401).json(user);
-            }
-        } catch (error) {
-            console.error("Error in UserController.login: ", error.message);
-            return res.status(500).json({
-                success: false,
-                message: "Internal server error"
-            })
-        }
-    }
-
+  async logout(req, res) {
+    try {
+      const result = UserServices.logout(res);
+      if (result.success) {
+        return res.json(200).json(result);
+      } else {
+        return res.json(400).json(result);
+      }
+    } catch (error) {} 
+  }
 }
 
-export default new UserController(UserService);
+export default new UserController(UserServices);
