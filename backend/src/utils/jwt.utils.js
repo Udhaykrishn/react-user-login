fb395e37feec7100d6258ad1ae634df1ac1ad008
arjuxlal <arjunlal543k210@gmail.com>
2024-12-28 19:43:00 +0530

    added private fields

--- backend/src/utils/jwt.utils.js
+++ backend/src/utils/jwt.utils.js
@@ -1,19 +1,24 @@
 import jwt from "jsonwebtoken";
 
 class JWTService {
-  constructor(secrm, exp = "1h") {
-    this.secrm = secrm;
-    this.exp = exp;
+  #secret;
+  #expire;
+  constructor(secret, expire = "1h") {
+    this.#secret = secret;
+    this.#expire = expire;
   }
 
   generateToken(payload) {
-    return jwt.sign(payload, this.secrm, { expiresIn: this.exp });
+    return jwt.sign(payload, this.#secret, { expiresIn: this.#expire });
   }
 
   verifyToken(token) {
     try {
-      return jwt.verify(token, this.secrm);
+      return jwt.verify(token, this.#secret);
     } catch (error) {
+      if (error.name === "TokenExpiredError") {
+        throw new Error("Token has expired");
+      }
       throw new Error("Invalid or expired token");
     }
   }
