1798b1b0b8b9063b2d3e5a59a02c5ee614c46636
arjuxlal <arjunlal543k210@gmail.com>
2024-12-28 21:47:10 +0530

    added the user router and using in app.use for access in application level

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,27 +1,27 @@
-import express from "express"
-import ConnectDB from "#config/db.config.js"
+import express from "express";
+import ConnectDB from "#config/db.config.js";
 import ENV from "#env/const.env.js";
 
-import UserRouter from "#router/user.routers.js"
+import UserRouter from "#router/user.routers.js";
 
-const app = express();
+const app = express(); 
 
-
-app.use(express.json())
-
-
-app.get("/user",UserRouter)
+app.use(express.json());
 
 const bootstrap = async () => {
-    try {
-        await ConnectDB.connect();
+  try {
+    await ConnectDB.connect();
 
-        const PORT = ENV.PORT
+    app.use("/user", UserRouter);
 
-        app.listen(PORT, () => console.log("server is started ",PORT))
-    } catch (error) {
-        console.error(`Failed to start server due to database connection error: ${error.message}`)
-    }
-}
+    const PORT = ENV.PORT;
+
+    app.listen(PORT, () => console.log("server is started ", PORT));
+  } catch (error) {
+    console.error(
+      `Failed to start server due to database connection error: ${error.message}`
+    );
+  }
+};
 
 bootstrap();
