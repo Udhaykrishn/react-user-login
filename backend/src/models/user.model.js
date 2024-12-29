ab9b17f8823f62c1fe586e2c68d4b836d97f51a2
Udhaykrishn <krishnauday320@gmail.com>
2024-12-29 22:52:37 +0530

    hashing password before saving

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,41 +1,51 @@
-import mongoose from "mongoose";
+import passwordService from '#util/password.utils.js';
+import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      minLength: 1,
-      maxLength: 50, 
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-      minLength: 8, 
-    },
-    role: {
-      type: String,
-      enum: ["user", "admin"],
-      default: "user",
-    },
-    isBlocked: {
-      type: Boolean,
-      default: false, 
-    },
-    photo: {
-      type: String,
-      default: "", 
-    },
-  },
-  {
-    timestamps: true,
-  }
+	{
+		username: {
+			type: String,
+			required: true,
+			minLength: 1,
+			maxLength: 50
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true
+		},
+		password: {
+			type: String,
+			required: true,
+			minLength: 8
+		},
+		role: {
+			type: String,
+			enum: [ 'user', 'admin' ],
+			default: 'user'
+		},
+		isBlocked: {
+			type: Boolean,
+			default: false
+		},
+		photo: {
+			type: String,
+			default: ''
+		}
+	},
+	{
+		timestamps: true
+	}
 );
 
-const User = mongoose.model("User", userSchema); 
-export {User};
+userSchema.pre('save', async function(next) {
+	if (!this.isModified('password')) {
+		return next();
+	}
+
+	this.password = await passwordService.hash(this.password);
+	next();
+});
+
+const User = mongoose.model('User', userSchema);
+export { User };
