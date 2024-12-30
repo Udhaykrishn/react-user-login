35761534472882f705fe7391c8de2a81882df3a9
Udhaykrishn <krishnauday320@gmail.com>
2024-12-31 03:01:20 +0530

    do it DI for all services

---
+++ backend/src/container/admin.container.js
@@ -0,0 +1,12 @@
+import Admin from '#model/admin.model.js';
+import { AdminRepository } from '#repository/auth.repository.js';
+import { AdminServices } from '#service/admin.service.js';
+import { AdminController } from '#controller/admin.controller.js';
+
+const adminRepo  = new AdminRepository(Admin)
+const adminService = new AdminServices(adminRepo)
+const adminController = new AdminController(adminService)
+
+export const adminControllers = {
+    login:adminController.login.bind(adminController)
+}
