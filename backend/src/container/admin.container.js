aaed75a3a99560da4a770ee65b6254161ff0a017
Udhaykrishn <krishnauday320@gmail.com>
2025-01-01 18:39:54 +0530

    DI using awilix

--- backend/src/container/admin.container.js
+++ backend/src/container/admin.container.js
@@ -1,18 +1,34 @@
 import Admin from '#model/admin.model.js';
-import User from "#model/user.model.js"
+import User from '#model/user.model.js';
 import { AdminRepository, UserRepository } from '#repository/index.js';
 import { AdminServices } from '#service/index.js';
 import { AdminController } from '#controller/index.js';
+import { createContainer, asClass, asValue } from 'awilix';
 
-const adminRepo  = new AdminRepository(Admin)
-const userRepo = new UserRepository(User)
-const adminService = new AdminServices(adminRepo,userRepo)
-const adminController = new AdminController(adminService)
+const container = createContainer();
+
+container.register({
+	//Models
+	adminModel: asValue(Admin),
+	userModel: asValue(User),
+
+	//Reposittory
+	adminRepository: asClass(AdminRepository).singleton(),
+	userRepository: asClass(UserRepository).singleton(),
+
+	//Services
+	adminServices: asClass(AdminServices).singleton(),
+
+	//Controllers
+	adminController: asClass(AdminController).singleton()
+});
+
+const adminController = container.resolve('adminController');
 
 export const adminControllers = {
-    login:adminController.login.bind(adminController),
-    create:adminController.create.bind(adminController),
-    update:adminController.update.bind(adminController),
-    delete:adminController.delete.bind(adminController),
-    users:adminController.users.bind(adminController)
-}
+	login: adminController.login.bind(adminController),
+	create: adminController.create.bind(adminController),
+	update: adminController.update.bind(adminController),
+	delete: adminController.delete.bind(adminController),
+	users: adminController.users.bind(adminController)
+};
