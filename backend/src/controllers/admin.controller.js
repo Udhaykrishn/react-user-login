e653d1d1c2ade1dc9d09eed4f9473b50035e3297
Udhaykrishn <krishnauday320@gmail.com>
2025-01-01 15:21:10 +0530

    create new user controller

--- backend/src/controllers/admin.controller.js
+++ backend/src/controllers/admin.controller.js
@@ -17,7 +17,21 @@ export class AdminController{
         } catch (error) {
             console.error(error.message)
             console.log(error)
-            return res.json(500).json({message:"Interval server error"})
+            return res.status(500).json({message:"Interval server error"})
+        }
+    }
+    async create(req,res){
+        try {
+            const user = await this.#adminService.create(req.body)
+
+            if(user.success){
+                return res.status(201).json(user)
+            }else{
+                return res.status(400).json(user)
+            }
+        } catch (error) {
+            console.log(error.message)
+            return res.status(500).json({message:"Interval server error"})
         }
     }
 }
