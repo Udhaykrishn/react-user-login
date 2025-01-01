78bb1095167bd7bb33be5b5d8e5b7d822466da6c
Udhaykrishn <krishnauday320@gmail.com>
2025-01-01 16:01:13 +0530

    rename user repo file

---
+++ backend/src/repositorys/user.repository.js
@@ -0,0 +1,33 @@
+ class UserRepository {
+  #user;
+  constructor(User) {
+    this.#user = User;
+  }
+
+  async create(payload) {
+    return await this.#user.create(payload);
+  }
+
+  async getAllUser() {
+    return await this.#user.find({});
+  }
+
+  async getUserById(id) {
+    return await this.#user.findById(id);
+  }
+
+  async getUserByEmail(email) {
+    return await this.#user.findOne({ email });
+  }
+
+  async updateUserById(id, payload) {
+    const options = { new: true, upsert: false };
+    return await this.#user.findByIdAndUpdate(id, payload, options);
+  }
+
+  async deleteUserById(id) {
+    return await this.#user.findByIdAndDelete(id);
+  }
+}
+
+export {UserRepository}
