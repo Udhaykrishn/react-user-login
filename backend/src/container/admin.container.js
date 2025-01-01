653c21d851f68f6c8ff0fd21a7b3bbc1e3a22b0f
Udhaykrishn <krishnauday320@gmail.com>
2025-01-01 15:03:04 +0530

    change the imported paths

--- backend/src/container/admin.container.js
+++ backend/src/container/admin.container.js
@@ -1,7 +1,7 @@
 import Admin from '#model/admin.model.js';
-import { AdminRepository } from '#repository/admin.repository.js';
-import { AdminServices } from '#service/admin.service.js';
-import { AdminController } from '#controller/admin.controller.js';
+import { AdminRepository } from '#repository/index.js';
+import { AdminServices } from '#service/index.js';
+import { AdminController } from '#controller/index.js';
 
 const adminRepo  = new AdminRepository(Admin)
 const adminService = new AdminServices(adminRepo)
