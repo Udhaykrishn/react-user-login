68756f30c91aa61ca4e1b1c87515d785fa52ca0a
Udhaykrishn <krishnauday320@gmail.com>
2025-01-02 10:20:11 +0530

    added admin logout api and added middleware check for authentication

--- backend/src/routers/admin.routes.js
+++ backend/src/routers/admin.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { adminControllers } from "#container/admin.container.js";
+import AdminAuthService from "#middleware/admin.middleware.js"
 
 class AdminRouter{
     #router;
@@ -11,11 +12,10 @@ class AdminRouter{
     #initRouter(){
         // auth routes
         this.#router.post("/login",adminControllers.login)
-        // this.#router.get("/dashbaord/logout")
+        this.#router.post("/dashboard/logout",adminControllers.logout)
 
         // crud routes
-        // this.#router.get("/dashboard",this.#adminController)
-        this.#router.get("/dashboard/users",adminControllers.users)
+        this.#router.get("/dashboard/users",AdminAuthService.adminAuth,adminControllers.users)
         this.#router.post("/dashboard/create",adminControllers.create)
         this.#router.patch("/dashboard/update/:id",adminControllers.update)
         this.#router.delete("/dashboard/delete/:id",adminControllers.delete)
