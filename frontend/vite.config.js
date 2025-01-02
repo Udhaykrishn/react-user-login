9798c6f749c45b1b97f5f71c696afaae44a78d47
Udhaykrishn <krishnauday320@gmail.com>
2025-01-02 10:51:38 +0530

    added alias resolver in vite config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path"
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite"
+import { fileURLToPath } from "url"
 
-// https://vite.dev/config/
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
