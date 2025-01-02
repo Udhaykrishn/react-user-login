76784f3462bbbcdd1e57fbc76a16bf1e2720e9dc
Udhaykrishn <krishnauday320@gmail.com>
2025-01-03 01:24:07 +0530

    added the router pages for related admins

---
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -0,0 +1,32 @@
+import UserList from "@/components/user-tables";
+import { useEffect, useState } from "react";
+import axios from "axios"
+
+const Dashboard = () => {
+  const [data,setData] = useState([]);
+
+
+  const fetchData = async ()=>{
+     try {
+      const response = await axios.get("admin/dashboard/users")
+      console.log(response.data)
+      setData(response.data.users)
+     } catch (error) {
+      console.log(error.message)
+     }
+  }
+
+  useEffect(()=>{
+      fetchData()
+  },[])
+
+
+  return (
+    <div className="h-screen">
+      <h1 className="text-center text-xl font-semibold mb-4">User Table</h1>
+      <UserList data={data} />
+    </div>
+  );
+}
+
+export default Dashboard
