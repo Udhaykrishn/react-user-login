1e163e96f41b4202ffede44b1646bd7aacb72d1f
Udhaykrishn <krishnauday320@gmail.com>
2025-01-02 10:23:20 +0530

    added admin auth middleware

---
+++ backend/src/middleware/admin.middleware.js
@@ -0,0 +1,30 @@
+import JwtService from "#util/jwt.utils.js";
+
+class AdminVerifyService{
+    async adminAuth(req,res,next){
+        const token = req.cookies?.adminToken
+        
+        if(!token){
+            return res.status(401).json({message:"Token missing,Please login later"})
+        }
+
+        try {
+            const payload = JwtService.verifyToken(token)
+            console.log(payload)
+            
+            if(payload.role !== "admin"){
+                return res.status(403).json({
+                    success:false,message:"Access denied, User does not have admin access"
+                })
+            }
+
+            return next()
+
+        } catch (error) {
+            console.log(error.message)
+            return res.status(500).json({success:false,message:"Internal server error"})
+        }
+    }
+}
+
+export default new AdminVerifyService()
