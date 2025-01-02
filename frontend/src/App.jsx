b4aa44071e06fad17342cbcf2941ff2873737fd7
Udhaykrishn <krishnauday320@gmail.com>
2025-01-03 01:18:23 +0530

    added the routes for admins and user only for login and signup with dashboard only

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,31 @@
-import { Button } from '@/components/ui/button'
-import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import {  Suspense } from 'react'
+import Login from "@/pages/users/Login"
+import Register from "@/pages/users/Signup"
+
+import Dashboard from '@/pages/admin/Dashboard'
+import Navbar from '@/components/Navbar'
+import AdminLogin from "@/pages/admin/Login"
 
 function App() {
 
   return (
-    <>
-      <Button>Click Me</Button>
-    </>
+    <div >
+        <Navbar/>
+        <Suspense>
+            <Routes>
+                  <Route path='/' element={<>Hello world</>} />
+                  <Route path='/user/login' element={<Login />}  />
+                   <Route path='/user/register' element={<Register/>} />
+                  <Route path='/admin' element={<Dashboard/>}  />
+                  <Route path='/admin/login' element={<AdminLogin />} />
+                  <Route path='/admin/logout'  />
+                  <Route path='/admin/create'  />
+                  <Route path='/admin/update/:id'  /> 
+                  {/* <Route path='/admin/delete/:id'  />  */} 
+            </Routes>
+        </Suspense>
+    </div>
   )
 }
 
