626ee7e2017b6381e1d1e10e5c0beff033334a9d
Udhaykrishn <krishnauday320@gmail.com>
2025-01-04 01:33:25 +0530

    for updated the supabase url and keys

--- backend/src/ENV/const.env.js
+++ backend/src/ENV/const.env.js
@@ -5,6 +5,8 @@ const ENV = {
     MONGODB_URL: process.env.MONGODB_URL,
     PORT: process.env.PORT,
     JWT_SECRET: process.env.JWT_SECRET,
+    SUPABASE_URL:process.env.SUPABASE_URL,
+    SUPABASE_API_KEY:process.env.SUPABASE_API_KEY
 }
 
 export default ENV
