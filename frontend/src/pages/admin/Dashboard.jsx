fec2e209ed23299e5a160b422de040eec7312f53
Udhaykrishn <krishnauday320@gmail.com>
2025-01-04 16:24:33 +0530

    make it easier compoent

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -1,37 +1,13 @@
+import { useProfile } from "@/components/admins/hooks";
 import UserList from "@/components/admins/user-tables";
-import { useEffect, useState } from "react";
-import axios from "axios"
-import { useNavigate } from "react-router-dom";
-
 const Dashboard = () => {
-  const [data,setData] = useState([]);
-  const navigate = useNavigate();
 
-
-  const fetchData = async ()=>{
-     try {
-      const response = await axios.get("admin/dashboard/users")
-      console.log(response.data)
-      setData(response.data.users)
-     } catch (error) {
-      if(error.response){
-        if(error.response?.data?.message === "TokenExpire"){
-          navigate("/admin/login")
-        }
-      }
-      console.log(error.message)
-     }
-  }
-
-  useEffect(()=>{
-      fetchData()
-  },[])
-
+  const { data, setUserList } = useProfile()
 
   return (
     <div className="h-screen">
       <h1 className="text-center text-xl font-semibold mb-4">User Table</h1>
-      <UserList data={data} setUserList={setData} />
+      <UserList data={data} setUserList={setUserList}  />
     </div>
   );
 }
