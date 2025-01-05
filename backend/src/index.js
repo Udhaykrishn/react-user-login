144c209578954c28d113c3b1f38b9505b8f474d1
Udhaykrishn <krishnauday320@gmail.com>
2025-01-06 02:18:02 +0530

    change the order of middleware packagees

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,10 +11,10 @@ import AdminRouter from "#router/admin.routes.js"
 
 const app = express();
 
-app.use(express.json());
 app.use(helmet())
-app.use(limiter)
+app.use(express.json());
 app.use(cors({ origin:"http://localhost:5173", credentials: true }))
+app.use(limiter)
 app.use(cookie())
 
 
