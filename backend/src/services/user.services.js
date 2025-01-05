592710a9a65542a01c39dc60c17bc2856143d452
Udhaykrishn <krishnauday320@gmail.com>
2025-01-05 22:03:18 +0530

    fix the bug and updated the user updation for password and remove some logs

--- backend/src/services/user.services.js
+++ backend/src/services/user.services.js
@@ -2,20 +2,17 @@ import { UserJoiSchema, userUpdateJoiSchema } from "#schema/user.schema.js";
 import PasswordHash from "#util/password.utils.js";
 import UserData from "#util/user.util.js";
 import JWT from "#util/jwt.utils.js";
-import path from "node:path"
-import { supabase } from "#config/supabase.config.js";
+import passwordService from "#util/password.utils.js";
 export class UserServices {
   #userRepository;
-constructor({userRepository}) {
+  constructor({ userRepository }) {
     this.#userRepository = userRepository;
   }
 
   #FormValidate(payload) {
     const { error } = UserJoiSchema.validate(payload);
     if (error) {
-      let err = error.details[0].message
-      console.log(err)
-      return {success:false,message:(error.details[0].message)};
+      return { success: false, message: (error.details[0].message) };
     }
     return null;
   }
@@ -23,12 +20,10 @@ constructor({userRepository}) {
   async register(payload) {
     try {
       const validationError = this.#FormValidate(payload)
-      if(validationError){
+      if (validationError) {
         return validationError
       }
-      const existingUser = await this.#userRepository.getUserByEmail(
-        payload.email
-      );
+      const existingUser = await this.#userRepository.getUserByEmail(payload.email);
 
       if (existingUser) {
         return { success: false, message: "User already exists" };
@@ -51,9 +46,9 @@ constructor({userRepository}) {
 
   async login(payload) {
     const validationError = this.#FormValidate(payload)
-      if(validationError){
-        return validationError
-      }
+    if (validationError) {
+      return validationError
+    }
     try {
       const user = await this.#userRepository.getUserByEmail(payload.email);
 
@@ -65,13 +60,14 @@ constructor({userRepository}) {
         return { success: false, message: "User blocked by admin" };
       }
 
-      const hashedPassword = PasswordHash.verify(
-        user.password,
-        payload.password
-      );
+      console.log(user.password, payload.password)
 
-      if (!hashedPassword) {
-        return { success: false, message: "Incorrect password!" };
+      const isPasswordMatch = await PasswordHash.verify(user.password, payload.password);
+
+      console.log(isPasswordMatch)
+
+      if (!isPasswordMatch) {
+        return { success: false, message: "incorrect password!" };
       }
 
       const tokenPayload = UserData.extractTokenPayload(user);
@@ -84,22 +80,25 @@ constructor({userRepository}) {
       console.log(error);
 
       return { success: false, message: `Error during login,Please try again` };
-    } 
+    }
   }
 
   async update(id, payload) {
     try {
       const { error } = userUpdateJoiSchema.validate(payload);
       if (error) {
-        console.log(error.details[0].message);
         return { success: false, message: error.details[0].message };
       }
-  
+      if (payload.password) {
+        payload.password = await passwordService.hash(payload.password)
+      }
+
+
       const updateUser = await this.#userRepository.updateUserById(id, payload);
       if (!updateUser) {
         return { success: false, message: "User not found" };
       }
-  
+
       return { success: true, message: "User updated successfully", user: updateUser };
     } catch (error) {
       console.error(error.message);
@@ -107,28 +106,33 @@ constructor({userRepository}) {
     }
   }
 
-  async profile(req){
+  async profile(req) {
     try {
       const user = await this.#userRepository.getUserById(req.user.id)
 
-      if(!user){
-        return {success:false,message:"User not found"}
+      if (!user) {
+        return { success: false, message: "User not found" }
       }
 
-      return {success:true,message:"User logged is ",user}
+      user.password  = null;
+
+      console.log(user,"user information from profile")
+
+      return { success: true, message: "User logged is ", user }
 
     } catch (error) {
       console.log(error)
-      return {success:false,message:"Internal server error"}
+      return { success: false, message: "Internal server error" }
     }
   }
-  
+
 
   async logout(res) {
     try {
-      res.clearCookie("userToken",{
-        httpOnly:true,
-        sameSite:"strict"
+      console.log("working this")
+      res.clearCookie("userToken", {
+        httpOnly: true,
+        sameSite: "strict"
       });
       return { success: true, message: "User logout successfully" };
     } catch (error) {
