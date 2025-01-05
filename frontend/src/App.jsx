b8583fe935faaca5fc68b3f5ae4e3369955e542b
Udhaykrishn <krishnauday320@gmail.com>
2025-01-05 22:05:54 +0530

    added admin procted routes and added admin and user checking

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import { Route, Routes } from 'react-router-dom'
-import { Suspense } from 'react'
+import { Route, Routes, useNavigate } from 'react-router-dom'
+import { Suspense, useEffect } from 'react'
 import Login from "@/pages/users/Login"
 import Register from "@/pages/users/Signup"
 
@@ -9,23 +9,46 @@ import AdminLogin from "@/pages/admin/Login"
 import Profile from "@/pages/users/Profile"
 import { Toaster } from '@/components/ui/toaster'
 
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchUserProfile } from './slice/user/userProfile'
+import { ProtectedRoute, PublicRoute } from './components/routes/UserAuthRouter'
+import { AdminProctedRoute, AdminPublicRoute } from './components/routes'
+
 
 function App() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const isAdmin = useSelector(state=>state.adminAuth.isAdmin)
+
+  useEffect(() => {
+    if(isAdmin){
+      navigate("/admin")
+    }
+    dispatch(fetchUserProfile());
+  }, [dispatch,isAdmin,navigate])
 
   return (
     <div >
       <Navbar />
-      <Toaster/>
+      <Toaster />
       <Suspense>
         <Routes>
-          <Route path='/' element={<Profile />} />
-          <Route path='/user/login' element={<Login />} />
-          <Route path='/user/register' element={<Register />} />
-          <Route path='/admin' element={<Dashboard />} />
-          <Route path='/admin/login' element={<AdminLogin />} />
+          <Route element={<AdminPublicRoute />}>
+            <Route path='/admin/login' element={<AdminLogin />} />
+          </Route>
+          <Route element={<PublicRoute />}>
+            <Route path='/user/login' element={<Login />} />
+            <Route path='/user/register' element={<Register />} />
+          </Route>
+          <Route element={<AdminProctedRoute />}>
+            <Route path='/admin' element={<Dashboard />} />
+          </Route>
+          <Route element={<ProtectedRoute />}>
+            <Route path='/' element={<Profile />} />
+          </Route>
         </Routes>
       </Suspense>
-     
+
     </div>
   )
 }
