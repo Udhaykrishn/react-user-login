f4a1a9d6b859e0e9e6f518471fc4cab155607408
Udhaykrishn <krishnauday320@gmail.com>
2025-01-05 23:00:47 +0530

    admin reducer added redux store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "@/slice/user/userSlice"
+import userProfileReducer from "@/slice/user/userProfile"
+import userAuthReducer  from "@/slice/user/userAuth";
+import adminAuthReducer from "@/slice/admin/adminAuth"
 
 export const store = configureStore({
     reducer: {
         users: userReducer,
+        userProfile: userProfileReducer,
+        userAuth:userAuthReducer,
+        adminAuth:adminAuthReducer
     }
 })
