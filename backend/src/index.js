1cacf51819c2e2dfc6b8b7af2cc8f5703b2454b4
Udhaykrishn <krishnauday320@gmail.com>
2025-01-08 17:24:36 +0530

    added the frontend url in env

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(helmet())
 app.use(express.json());
-app.use(cors({ origin:"http://localhost:5173", credentials: true }))
+app.use(cors({ origin:process.env.FRONTEND_URI, credentials: true }))
 app.use(limiter)
 app.use(cookie())
 
